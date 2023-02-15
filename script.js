ef9f274c39a5a458217c63a1f8fdb8a99531dbd3
David Andersson <david.andersson@zocom.se>
2023-02-15 11:29:45 +0100

    Better comments

--- script.js
+++ script.js
@@ -5,34 +5,36 @@
 const ajaxButton = document.querySelector('#ajax-button')
 const ajaxPara = document.querySelector('#ajax-response')
 
-// Variables and state
+// Variabler och state
 const baseUrl = 'https://forverkliga.se/JavaScript/api/simple.php'
 
 
 // Event listeners
 ajaxButton.addEventListener('click', async () => {
+	// performance.now() används för att mäta hur lång tid det tar att skicka request och vänta på svaret.
 	const startTime = performance.now()
-	console.log('Ajax button click 1')
+	// Använd console.log för felsökning när det behövs
+	// console.log('Ajax button click 1')
 	
+	// Ett vanligt request som skickas med fetch behöver inga inställningar. Man kan utelämna options: await fetch(baseUrl)
 	const options = {}
 	let response = await fetch(baseUrl, options)
-	console.log('Ajax button click 2', response)
+	// console.log('Ajax button click 2', response)
 
-	
 	let data = await response.json()
-	console.log('Ajax button click 3', data)
-	
-	// ajaxPara.innerText = JSON.stringify(data)
-	// ajaxPara.innerText = data.message
-	// ajaxPara.innerText = 'hejsan'
+	// console.log('Ajax button click 3', data)
 
+	// Räkna ut hur lång tid det tog att köra funktionen
 	let endTime = performance.now()
 	let diff = endTime - startTime
 
+	// Presentera datan för användaren
 	ajaxPara.innerHTML = `${data.message} <br/> The request took ${diff} ms to execute.`
 })
 
 
 // När man hovrar över fetch med musen säger VS Code:
 // function fetch(input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>
-// :Promise<Response> betyder att funktionen returnerar ett PROMISE
+// :Promise<Response> betyder att funktionen returnerar ett PROMISE, som innehåller Response
+// Ett Promise kommer så småningom att sluta med "resolve" eller "reject"
+// Få ut Response ur Promise genom att vänta med AWAIT
