1033fbc7184a6630b14269edd9b4659800da85ce
David Andersson <david.andersson@zocom.se>
2023-02-15 14:44:27 +0100

    Safer code

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const exercise1 = {
 	output: document.querySelector('#exercise1-output'),
 	list: document.querySelector('#exercise1-advice-list')
 }
+const safe = {
+	btn: document.querySelector('#safe-button'),
+	output: document.querySelector('#safe-output')
+}
+	// < button id = "safe-button" > Skicka request </button>
+	// 	<p id="safe-output"></
 
 // Variabler och state
 const baseUrl = 'https://forverkliga.se/JavaScript/api/simple.php'
@@ -68,6 +74,38 @@ exercise1.btn.addEventListener('click', async () => {
 	li.append(adviceSpan)
 	exercise1.list.append(li)
 })
+
+
+safe.btn.addEventListener('click', async () => {
+	try {
+		// Försök att göra något, som inte är garanterat att det lyckas
+		// Detta API förväntar sig querystring "key=value"
+		const response = await fetch('https://forverkliga.se/JavaScript/api/simple.php?key=value')
+
+		const data = await response.json()
+		// await response.text() - om man bara vill ha innehållet som text
+
+		console.log('Safe | data=', data)
+		if( data && data.message && data.time ) {
+			safe.output.innerText = `Meddelande: ${data.message}. Tid: ${data.time}.`
+		} else {
+			safe.output.innerText = 'Ingen massage från servern, tyvärr. Försök igen senare.'
+		}
+
+	} catch(error) {
+		safe.output.innerText = `Det gick inte att skicka request! Försök igen senare. Felmeddelande: ` + error.message
+	}
+	// Vad kan vi göra om något går fel?
+	// Be användaren kontrollera om man är uppkopplad
+	// Be användaren försöka igen om en stund - kanske bäst
+	// Meddelande: "Failed to load" - korrekt, men hjälper inte användaren. Undvik!
+})
+/*
+JSON är ett objekt { parse, stringify } som används för att
+JSON.parse <- omvandla en sträng till JS-objekt
+JSON.stringify <- omvandla ett objekt till string
+response.json() är en metod som ingår i Response-objektet. Används eftersom vi inte har fått hela strängen från servern än.
+*/
 
 
 
